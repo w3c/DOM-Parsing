18b9f6673a7181b828e8c8c9a9c6a1b24ec0f50a
travil <travil@travil1.wingroup.windeploy.ntdev.microsoft.com>
2012-08-29 16:41:46 -0700

    Conversion to ReSpec and W3C editor's draft template.
    Also includes an update to WebIDL exceptions.

---
+++ respecConfig.js
@@ -0,0 +1,30 @@
+// Configuration for the Editor's Draft of DOM Parsing and Serialization
+
+var respecConfig = {
+	specStatus: "ED",
+    editors: [
+	    {
+			name: "Travis Leithead", 
+            company: "Microsoft Corp.",
+            url: "mailto:travis.leithead@microsoft.com?subject=[DOMParsing] Spec Feedback",
+            companyURL: "http://www.microsoft.com"
+		},
+        {
+			name: "Ms2ger",
+            url: "mailto:ms2ger@gmail.com"
+		}
+	],
+    //publishDate:  "2012-03-06",
+	edDraftURI: "http://dvcs.w3.org/hg/innerhtml/raw-file/tip/index.html",
+	previousPublishDate: null,
+    prevED: "http://html5.org/specs/dom-parsing.html",
+    noIDLIn:  true,
+    inlineCSS:  true,
+    //noRecTrack:  true,
+    shortName:  "innerhtml",
+    extraCSS: ["http://dev.w3.org/2009/dap/ReSpec.js/css/respec.css"],
+    wg:         ["Web Applications Working Group"],
+    wgURI:    ["http://www.w3.org/2008/webapps/"],
+    wgPublicList: "public-webapps",
+    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/42538/status"]
+};
