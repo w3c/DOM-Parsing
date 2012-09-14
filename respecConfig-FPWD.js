afb47dc8aa5487034590250db2e7d0fe55208a40
Travis Leithead <travisleithead@users.noreply.github.com>
2012-09-14 10:12:23 -0700

    Preparing the FPWD

---
+++ respecConfig-FPWD.js
@@ -0,0 +1,30 @@
+// Configuration for the Editor's Draft of DOM Parsing and Serialization
+
+var respecConfig = {
+	specStatus: "FPWD",
+    editors: [
+	    {
+			name: "Travis Leithead", 
+            company: "Microsoft Corp.",
+            url: "mailto:travis.leithead@microsoft.com?subject=[DOMParsing] Spec Feedback",
+            companyURL: "http://www.microsoft.com"
+		},
+        {
+			name: "Ms2ger",
+            url: "mailto:ms2ger@gmail.com"
+		}
+	],
+    publishDate:  "2012-09-20",
+	edDraftURI: "http://dvcs.w3.org/hg/innerhtml/raw-file/tip/index.html",
+	previousPublishDate: null,
+    prevED: "http://html5.org/specs/dom-parsing.html",
+    noIDLIn:  true,
+    inlineCSS:  true,
+    //noRecTrack:  true,
+    shortName:  "DOM-Parsing",
+    extraCSS: ["http://dev.w3.org/2009/dap/ReSpec.js/css/respec.css"],
+    wg:         ["Web Applications Working Group"],
+    wgURI:    ["http://www.w3.org/2008/webapps/"],
+    wgPublicList: "public-webapps",
+    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/42538/status"]
+};
