a20a87fdea690d2e52a1a174150849e1dc5f20e0
travil <travil@localhost>
2013-11-12 20:40:07 +0800

    Boilerplate update to latest respec features; added relevant links to WHATWG copy.

--- respecConfig.js
+++ respecConfig.js
@@ -1,25 +1,40 @@
 // Configuration for the Editor's Draft of DOM Parsing and Serialization
 
 var respecConfig = {
+    subtitle: "DOMParser, XMLSerializer, innerHTML, and similar APIs",
 	specStatus: "ED",
     editors: [
 	    {
 			name: "Travis Leithead", 
-            company: "Microsoft Corp.",
-            url: "mailto:travis.leithead@microsoft.com?subject=%5BDOMParsing%5D%20Spec%20Feedback",
+            company: "Microsoft",
+            mailto: "travis.leithead@microsoft.com",
             companyURL: "http://www.microsoft.com"
-		}
+		},
+        {
+            name: "Ms2ger",
+            company: "Mozilla",
+            companyURL: "http://www.mozilla.org",
+            note: "WHATWG Living Standard version"
+        }
 	],
     //publishDate:  "2012-09-20",
 	edDraftURI: "http://dvcs.w3.org/hg/innerhtml/raw-file/tip/index.html",
-	previousPublishDate: null,
-    prevED: "http://html5.org/specs/dom-parsing.html",
+	previousPublishDate: "2012-09-20",
+    previousMaturity: "2012-09-20",
+    otherLinks: [
+        {
+            key: "WHATWG Living Standard",
+            data: [{ value: "http://domparsing.spec.whatwg.org/", href: "http://domparsing.spec.whatwg.org/"}]
+        }
+    ],
     noIDLIn:  true,
     inlineCSS:  true,
     //noRecTrack:  true,
-    shortName:  "innerhtml",
+    shortName:  "DOM-Parsing",
     wg:         ["Web Applications Working Group"],
     wgURI:    ["http://www.w3.org/2008/webapps/"],
     wgPublicList: "www-dom",
-    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/42538/status"]
+    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/42538/status"],
+    subjectPrefix: "DOM-Parsing",
+    testSuiteURI: "https://github.com/whatwg/domparsing/tree/master/tests"
 };
