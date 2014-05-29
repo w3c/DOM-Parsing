1064a3f8eadf4a473428bff113fa54f6024219a0
Travis Leithead <travisleithead@users.noreply.github.com>
2014-05-29 13:39:45 -0700

    Create a framework implementation report and respec config file for CR

---
+++ respecConfig-CR.js
@@ -0,0 +1,30 @@
+// Configuration for the Editor's Draft of DOM Parsing and Serialization
+
+var respecConfig = {
+    subtitle: "DOMParser, XMLSerializer, innerHTML, and similar APIs",
+	specStatus: "CR",
+    publishDate: "2014-06-04",
+    editors: [
+	    {
+			name: "Travis Leithead", 
+            company: "Microsoft",
+            mailto: "travis.leithead@microsoft.com",
+            companyURL: "http://www.microsoft.com"
+		}
+	],
+	edDraftURI: "https://dvcs.w3.org/hg/innerhtml/raw-file/tip/index.html",
+	previousPublishDate: "2014-05-01",
+    previousMaturity: "LC",
+    crEnd: "2014-10-06",
+    implementationReportURI: "https://dvcs.w3.org/hg/innerhtml/raw-file/tip/implementationReport.html",
+    noIDLIn:  true,
+    inlineCSS:  true,
+    //noRecTrack:  true,
+    shortName:  "DOM-Parsing",
+    wg:         ["Web Applications Working Group"],
+    wgURI:    ["http://www.w3.org/2008/webapps/"],
+    wgPublicList: "www-dom",
+    wgPatentURI:  ["http://www.w3.org/2004/01/pp-impl/42538/status"],
+    subjectPrefix: "DOM-Parsing",
+    testSuiteURI: "http://w3c-test.org/domparsing/"
+};
